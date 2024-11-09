05246a4deb07b8679f9ac9a2a6c6019fd779229c
KK <kashiwagik@adm.ncn.ac.jp>
2024-11-10 00:12:09 +0900

    add prisma

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 import { auth, signIn, signOut } from "@/auth";
+import prisma from '@/lib/db'
 
 export default async function Home() {
   const session = await auth();
+  const students = await prisma.student.findMany()
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>home</h1>
@@ -28,6 +31,9 @@ export default async function Home() {
         </form>
       )}
       <pre>{JSON.stringify(session, null, 2)}</pre>
+      <pre>{JSON.stringify(students, null, 2)}</pre>
+
+
     </div>
   );
 }
